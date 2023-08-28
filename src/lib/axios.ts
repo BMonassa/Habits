b55a1d397600186446254809c54c3cb53a6d1ac6
Bruno Monassa <91953401+BMonassa@users.noreply.github.com>
2023-08-28 17:04:10 -0300

    Update axios.ts

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: 'http://192.168.1.6:3333'
+ baseURL: 'https://nodedeploy-habits.onrender.com'
   // baseURL: 'https://baconipsum.com'
   // baseURL: 'https://localhost:3333'
 });
